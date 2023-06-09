32b58d406f1bc481a93e4acc3d2c1d8f6e8a2ebc
matin sangabi <sangabiwork@gmail.com>
2023-06-09 04:44:43 -0700

    add global functional and insert toastify and complete the guessClickHandler

--- src/components/guesses/guessList.js
+++ src/components/guesses/guessList.js
@@ -1,12 +1,11 @@
 // import { commonString } from "@/utils/commonString";
 
 const GeuessList = ({ choose, value }) => {
-  console.log(value);
   return (
     <ul>
       {choose.map((item, index) => (
         <li key={index}>
-          <span>User : {commonString(item.user, value)}</span>
+          {/* <span>User : {commonString(item.user, value)}</span> */}
           <span>Robot : {item.robot}</span>
         </li>
       ))}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,57 +3,93 @@ import GuessWord from "@/components/guesses/guessWord";
 import ModalsChild from "@/components/modal/modal";
 import Modals from "@/components/modal/modalChild";
 import StartGame from "@/components/startButton";
+import {
+  EASY_MODE,
+  HARD_MODE,
+  MEDIUM_MODE,
+  ModeData,
+} from "@/utils/modeActions";
 import { selectRandomData } from "@/utils/randomDataSelect";
 import { getRandomValue } from "@/utils/randomValue";
 import { useState } from "react";
-
+import { toast } from "react-toastify";
 const HomePage = () => {
   // states
   const [modal, setModal] = useState(false);
   const [selectValue, setSelectValue] = useState("");
   const [guessValue, setGuessValue] = useState("");
+  const [choose, setChoose] = useState([]);
   const [randValue, setRandValue] = useState({
     value: "",
     index: "",
     mode: "",
   });
-  const [choose, setChoose] = useState([]);
-  //functional handle
 
+  //functional handle
   /// start and close game funcction
   const startGameHandleClick = () => {
     setModal(true);
     const randomValue = selectRandomData();
     setSelectValue(randomValue);
+    setChoose([]);
   };
   const closeGameHandler = () => {
-    setModal(false);
-    setGuessValue("");
-    setRandValue({ value: "", index: "", mode: "" });
+    resetState();
   };
   /// modal function
   const closeModalHandler = () => {
-    setModal(false);
-    setRandValue({ value: "", index: " ", mode: "" });
+    resetState();
   };
   const modalClickHandler = ({ target }) => {
     const { index, value, mode } = getRandomValue(selectValue, target.value);
     setRandValue({ value, index, mode });
     setModal(false);
   };
-
   /// guesses functional
   const guessChangeHandler = ({ target }) => {
     setGuessValue(target.value.toUpperCase());
   };
-
   const guessClickHandler = () => {
     const { mode, value, index } = randValue;
+    setGuessValue("");
+    console.log(value);
     if (guessValue === value) {
-      setRandValue({ value: "", index: "", mode: "" });
-      setChoose([]);
+      toast.success("you won !");
+      resetState();
+    }
+    switch (mode) {
+      case EASY_MODE: {
+        robotSelectData(index, 15, value, guessValue);
+        break;
+      }
+      case MEDIUM_MODE: {
+        robotSelectData(index, 7, value, guessValue);
+        break;
+      }
+      case HARD_MODE: {
+        robotSelectData(index, 2, value, guessValue);
+        break;
+      }
     }
   };
+  //global function
+  const robotSelectData = (index, sum, value, guessValue) => {
+    const randomData = ModeData(index, sum);
+    setChoose([...choose, { user: guessValue, robot: randomData }]);
+    if (randomData === value) {
+      toast.error("You Loose !");
+      resetState();
+      return;
+    }
+  };
+
+  function resetState() {
+    setModal(false);
+    setSelectValue("");
+    setGuessValue("");
+    setRandValue({ value: "", index: "", mode: "" });
+    setChoose([]);
+  }
 
   return (
     <main className="mian">
